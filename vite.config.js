252f1bdec958152a9e28a0ec1561e3f3d0cfd1b7
Suyash Shukla <suyashshukla@Suyashs-MacBook-Air.local>
2025-10-25 02:02:10 +0530

    Image fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/3d-animation",
+  base: "/3d-animation/",
 });
