46abbcfbd670b7a54009aeeacc7b627bb9be1859
Suyash Shukla <suyashshukla@Suyashs-MacBook-Air.local>
2025-10-25 02:17:57 +0530

    image fix

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -2,8 +2,9 @@ import React, { useRef } from "react";
 import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
+import sideImage from "./side.jpg";
 const Scene = () => {
-  const tex = useTexture("/side.jpg");
+  const tex = useTexture(sideImage);
   const scene = useRef(null);
   useFrame((state, delta) => {
     scene.current.rotation.y += delta;
